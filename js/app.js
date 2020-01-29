a3769688f09f32745221a2861841d94af4430a3c
AlexisDominguez <alexisarieldominguezsanchez@pop-os.localdomain>
2020-01-28 22:06:33 -0700

    Funcionalidad de mostrar error

--- js/app.js
+++ js/app.js
@@ -1,22 +1,40 @@
-// Importanciones
+// -- Importanciones -- 
 import Presupuesto from "../js/presupuesto.js";
 import Interfaz from "../js/interfaz.js";
 
-// Variables
+// -- Variables -- 
 const presupuestoUsuario = prompt("¿Cuál es tu presupuesto semanal?");
 let cantidadPresupuesto;
 
-// Event listeners
+const formulario = document.getElementById("agregar-gasto");
+
+// -- Event listeners --
+
+// Se encarga de establer las acciones justo al cargar la página
 document.addEventListener("DOMContentLoaded", function(){
    // Se encarga de verificar si el valor introducido en el prompt()
    // es  nulo o un espacio vacío.
    if(presupuestoUsuario === null || presupuestoUsuario === ""){
       window.location.reload();    //Recarga la página
    }else{
-      //Instancia de Presupuesto 
       cantidadPresupuesto = new Presupuesto(presupuestoUsuario);
-      //Instancia de Interfaz
       const ui = new Interfaz();
+
       ui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
    } 
+});
+
+// Se encarga de procesar las acciones del formulario
+formulario.addEventListener("submit", function(e){
+   e.preventDefault();
+
+   const nombreGasto = document.getElementById("gasto").value;
+   const cantidadGasto = document.getElementById("cantidad").value;
+   const ui = new Interfaz();
+
+   if(nombreGasto === "" || cantidadGasto === ""){
+      ui.mostrarMensaje("Error al crear gasto", "error");
+   }else{
+      ui.MostrarMensaje("Gasto añadido al listado", "success");
+   }
 });
--- js/interfaz.js
+++ js/interfaz.js
@@ -7,6 +7,27 @@ class Interfaz{
       presupuestoSpan.innerHTML = `${cantidad}`;
       restanteSpan.innerHTML = `${cantidad}`;
    }
+
+   // Crea un <div> mostrando un mensaje de error o success
+   mostrarMensaje(mensaje, tipo){
+      const div = document.createElement("div");
+      div.classList.add("text-center", "alert");   
+      if(tipo === "error"){
+         div.classList.add("alert-danger");
+      }else{
+         div.classList.add("alert-success");
+      }
+
+      div.innerHTML = mensaje;
+      const formulario = document.getElementById("agregar-gasto");
+
+      // Inserta el <div> justo antes del s
+      document.querySelector(".primario").insertBefore(div, formulario);
+
+      setTimeout(function(){
+         div.remove();
+      },3000);
+   }
 }
 
 export default Interfaz;
